7b6bc05c9b749cdd214f7c83f2f452c988c9697f
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-27 23:08:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,6 +25,7 @@ end
 
 def get_english_meaning(file_path, emoticon)
     load_library(file_path).each do |key, value|
+      binding.pry
         if value[:japanese] == emoticon 
             return key
         end
